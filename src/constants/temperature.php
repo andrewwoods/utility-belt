a88ac6eb3d0294797817db7123b8d1cf39f5cab2
Andrew Woods <atwoods1@gmail.com>
2019-06-17 15:36:58 -0700

    Add temperature constants

---
+++ src/constants/temperature.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of the Utility Belt package.
+ *
+ * @author Andrew Woods <andrew@andrewwoods.net>
+ * @copyright 2019 Andrew Woods
+ * @license https://opensource.org/licenses/MIT MIT License
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/*
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                             *
+ *                          Temperature Related Constants                      *
+ *                                                                             *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ */
+
+define('FREEZING_POINT_IN_CELSIUS', 0);
+
+define('FREEZING_POINT_IN_FAHRENHEIT', 32);
+
+define('BOILING_POINT_IN_CELSIUS', 100);
+
+define('BOILING_POINT_IN_FAHRENHEIT', 212);
